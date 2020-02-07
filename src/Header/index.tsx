fb8e5a38ca6bfa29f3d38b09ec1be28bb8df552d
f-koki <57610613+f-koki@users.noreply.github.com>
2020-02-07 19:25:04 +0900

    feat: arrange the header item

--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./Header.css";
 
 const Header: React.FC = () => (
-  <header className="Header">
-    <span className="inscription">just one by one</span>
-    <span className="profile">PROFILE</span>
-    <span className="works">WORKS</span>
-  </header>
+  <div className="Header">
+    <div className="inscription">just one by one</div>
+    <div className="profile">PROFILE</div>
+    <div className="works">WORKS</div>
+  </div>
 );
 
 export default Header;
