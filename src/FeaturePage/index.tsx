746a7da4b5e15c30bdc882ece6808d2c6ce29d78
f-koki <57610613+f-koki@users.noreply.github.com>
2020-02-17 20:53:28 +0900

    feat: write specific contents

--- src/FeaturePage/index.tsx
+++ src/FeaturePage/index.tsx
@@ -5,36 +5,32 @@ const FeaturesPage: React.FC = () => {
   return (
     <section className="text-center my-5">
       <h2 className="h1-responsive font-weight-bold my-5">
-        Why is it so great?
+        Works
       </h2>
-      <p className="lead grey-text w-responsive mx-auto mb-5">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam.
-      </p>
+      {/* <p className="lead grey-text w-responsive mx-auto mb-5">
+        I work
+      </p> */}
       <MDBRow>
         <MDBCol md="4">
           <MDBIcon icon="chart-area" size="3x" className="red-text" />
-          <h5 className="font-weight-bold my-4">Analytics</h5>
+          <h5 className="font-weight-bold my-4">ENGINEERING</h5>
           <p className="grey-text mb-md-0 mb-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Reprehenderit maiores aperiam minima assumenda deleniti hic.
+            フロントエンド開発
           </p>
         </MDBCol>
         <MDBCol md="4">
           <MDBIcon icon="book" size="3x" className="cyan-text" />
-          <h5 className="font-weight-bold my-4">Tutorials</h5>
+          <h5 className="font-weight-bold my-4">DESIGN</h5>
           <p className="grey-text mb-md-0 mb-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Reprehenderit maiores aperiam minima assumenda deleniti hic.
+            デザイン
           </p>
         </MDBCol>
         <MDBCol md="4">
           <MDBIcon far icon="comments" size="3x" className="orange-text" />
-          <h5 className="font-weight-bold my-4">Support</h5>
+          <h5 className="font-weight-bold my-4">KEYBOARDIST</h5>
           <p className="grey-text mb-md-0 mb-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Reprehenderit maiores aperiam minima assumenda deleniti hic.
+            Youtube登録者数1000人
+            ピアノ講座動画
           </p>
         </MDBCol>
       </MDBRow>
