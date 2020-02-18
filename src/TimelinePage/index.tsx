c3793339d4fe8135310e6f7eb863dff396a566f1
f-koki <57610613+f-koki@users.noreply.github.com>
2020-02-18 21:27:00 +0900

    feat: use more suitable template for timeline

--- src/TimelinePage/index.tsx
+++ src/TimelinePage/index.tsx
@@ -2,52 +2,66 @@ import React from 'react'
 import './TimelinePage.css'
 
 const TimelinePage: React.FC = () => (
-  <div className="row">
-  <div className="col-md-12">
-    <div className="main-timeline">
-      <a href="#" className="timeline">
-        <div className="timeline-icon"><i className="fa fa-globe"></i></div>
-        <div className="timeline-content">
-          <h3 className="title">Web Designer</h3>
-          <p className="description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ducimus officiis quod! Aperiam eveniet
-            nam nostrum odit quasi ullam voluptatum.
-          </p>
+  <div className="container">
+    <div className="timeline">
+      <div className="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+        <div className="col-10 col-md-5 order-3 order-md-1 timeline-content">
+          <h3 className=" text-light">Timeline Heading</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, eaque amet deleniti hic quas qui cumque
+            delectus aliquid, eius quia quod, quae, aliquam aspernatur facilis. Minima quod corporis dignissimos porro.
+        </p>
         </div>
-      </a>
-      <a href="#" className="timeline">
-        <div className="timeline-icon"><i className="fa fa-rocket"></i></div>
-        <div className="timeline-content">
-          <h3 className="title">Web Developer</h3>
-          <p className="description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ducimus officiis quod! Aperiam eveniet
-            nam nostrum odit quasi ullam voluptatum.
-          </p>
+        <div className="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
+          <img src="img/img13.png" className="img-fluid" alt="img" />
         </div>
-      </a>
-      <a href="#" className="timeline">
-        <div className="timeline-icon"><i className="fa fa-briefcase"></i></div>
-        <div className="timeline-content">
-          <h3 className="title">Web Designer</h3>
-          <p className="description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ducimus officiis quod! Aperiam eveniet
-            nam nostrum odit quasi ullam voluptatum.
-          </p>
+        <div className="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+          <time>2018-02-23</time>
         </div>
-      </a>
-      <a href="#" className="timeline">
-        <div className="timeline-icon"><i className="fa fa-mobile"></i></div>
-        <div className="timeline-content">
-          <h3 className="title">Web Developer</h3>
-          <p className="description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ducimus officiis quod! Aperiam eveniet
-            nam nostrum odit quasi ullam voluptatum.
-          </p>
+      </div>
+      <div className="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+        <div className="col-10 col-md-5 order-3 order-md-1 timeline-content">
+          <h3 className=" text-light">Timeline Heading</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, eaque amet deleniti hic quas qui cumque
+            delectus aliquid, eius quia quod, quae, aliquam aspernatur facilis. Minima quod corporis dignissimos porro.
+        </p>
         </div>
-      </a>
+        <div className="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
+          <img src="img/img13.png" className="img-fluid" alt="img" />
+        </div>
+        <div className="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+          <time>2018-02-23</time>
+        </div>
+      </div>
+      <div className="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+        <div className="col-10 col-md-5 order-3 order-md-1 timeline-content">
+          <h3 className=" text-light">Timeline Heading</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, eaque amet deleniti hic quas qui cumque
+            delectus aliquid, eius quia quod, quae, aliquam aspernatur facilis. Minima quod corporis dignissimos porro.
+        </p>
+        </div>
+        <div className="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
+          <img src="img/img13.png" className="img-fluid" alt="img" />
+        </div>
+        <div className="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+          <time>2018-02-23</time>
+        </div>
+      </div>
+      <div className="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+        <div className="col-10 col-md-5 order-3 order-md-1 timeline-content">
+          <h3 className=" text-light">Timeline Heading</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, eaque amet deleniti hic quas qui cumque
+            delectus aliquid, eius quia quod, quae, aliquam aspernatur facilis. Minima quod corporis dignissimos porro.
+        </p>
+        </div>
+        <div className="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
+          <img src="img/img13.png" className="img-fluid" alt="img" />
+        </div>
+        <div className="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+          <time>2018-02-23</time>
+        </div>
+      </div>
     </div>
   </div>
-</div>
 )
 
 export default TimelinePage
