c8ee42dd90847b66aac27c23a35285a21c17f636
f-koki <57610613+f-koki@users.noreply.github.com>
2020-02-21 20:50:49 +0900

    feat: update skill template

--- src/FeaturePage/index.tsx
+++ src/FeaturePage/index.tsx
@@ -3,35 +3,108 @@ import { MDBRow, MDBCol, MDBIcon } from "mdbreact";
 
 const FeaturesPage: React.FC = () => {
   return (
-    <section className="text-center my-5">
-      <h2 className="h1-responsive font-weight-bold my-5">
-        Works
-      </h2>
-      {/* <p className="lead grey-text w-responsive mx-auto mb-5">
-        I work
-      </p> */}
+    <section className="my-5">
+      <h2 className="h1-responsive font-weight-bold text-center my-5">
+        Why is it so great?
+        </h2>
+      <p className="lead grey-text w-responsive text-center mx-auto mb-5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+        ad minim veniam.
+        </p>
       <MDBRow>
         <MDBCol md="4">
-          <MDBIcon icon="chart-area" size="3x" className="red-text" />
-          <h5 className="font-weight-bold my-4">ENGINEERING</h5>
-          <p className="grey-text mb-md-0 mb-5">
-            フロントエンド開発
-          </p>
+          <MDBRow className="mb-3">
+            <MDBCol size="2">
+              <MDBIcon
+                icon="flag-checkered"
+                size="2x"
+                className="deep-purple-text"
+              />
+            </MDBCol>
+            <MDBCol size="10">
+              <h5 className="font-weight-bold mb-3">International</h5>
+              <p className="grey-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reprehenderit maiores nam, aperiam minima assumenda deleniti
+                hic.
+                </p>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className="mb-3">
+            <MDBCol size="2">
+              <MDBIcon icon="flask" size="2x" className="deep-purple-text" />
+            </MDBCol>
+            <MDBCol size="10">
+              <h5 className="font-weight-bold mb-3">Experimental</h5>
+              <p className="grey-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reprehenderit maiores nam, aperiam minima assumenda deleniti
+                hic.
+                </p>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className="mb-3">
+            <MDBCol size="2">
+              <MDBIcon icon="glass-martini" size="2x" className="deep-purple-text" />
+            </MDBCol>
+            <MDBCol size="10">
+              <h5 className="font-weight-bold mb-3">Relaxing</h5>
+              <p className="grey-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reprehenderit maiores nam, aperiam minima assumenda deleniti
+                hic.
+                </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBCol>
+        <MDBCol md="4" className="text-name">
+          <img
+            className="img-fluid"
+            src="https://mdbootstrap.com/img/Mockups/Transparent/Small/iphone-portfolio1.png"
+            alt=""
+          />
         </MDBCol>
         <MDBCol md="4">
-          <MDBIcon icon="book" size="3x" className="cyan-text" />
-          <h5 className="font-weight-bold my-4">DESIGN</h5>
-          <p className="grey-text mb-md-0 mb-5">
-            デザイン
-          </p>
-        </MDBCol>
-        <MDBCol md="4">
-          <MDBIcon far icon="comments" size="3x" className="orange-text" />
-          <h5 className="font-weight-bold my-4">KEYBOARDIST</h5>
-          <p className="grey-text mb-md-0 mb-5">
-            Youtube登録者数1000人
-            ピアノ講座動画
-          </p>
+          <MDBRow className="mb-3">
+            <MDBCol size="2">
+              <MDBIcon icon="heart" size="2x" className="deep-purple-text" />
+            </MDBCol>
+            <MDBCol size="10">
+              <h5 className="font-weight-bold mb-3">Beloved</h5>
+              <p className="grey-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reprehenderit maiores nam, aperiam minima assumenda deleniti
+                hic.
+                </p>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className="mb-3">
+            <MDBCol size="2">
+              <MDBIcon icon="bolt" size="2x" className="deep-purple-text" />
+            </MDBCol>
+            <MDBCol size="10">
+              <h5 className="font-weight-bold mb-3">Rapid</h5>
+              <p className="grey-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reprehenderit maiores nam, aperiam minima assumenda deleniti
+                hic.
+                </p>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className="mb-3">
+            <MDBCol size="2">
+              <MDBIcon icon="magic" size="2x" className="deep-purple-text" />
+            </MDBCol>
+            <MDBCol size="10">
+              <h5 className="font-weight-bold mb-3">Magical</h5>
+              <p className="grey-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reprehenderit maiores nam, aperiam minima assumenda deleniti
+                hic.
+                </p>
+            </MDBCol>
+          </MDBRow>
         </MDBCol>
       </MDBRow>
     </section>
