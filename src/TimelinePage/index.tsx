d18e00993e965ee46d0f17f7b14698d96372b5d5
f-koki <fukushimakoki0108@gmail.com>
2020-02-22 23:28:04 +0900

    fix: fix the description

--- src/TimelinePage/index.tsx
+++ src/TimelinePage/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import './TimelinePage.css'
+import React from "react";
+import "./TimelinePage.css";
 
 const TimelinePage: React.FC = () => (
   <div className="container">
     <div className="timeline">
       <div className="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
         <div className="col-10 col-md-5 order-3 order-md-1 timeline-content">
-          <h3 className=" text-light">大学受験失敗</h3>
-          <p>記憶術に魅了され、受験失敗。
-        </p>
+          <h3 className=" text-light">幼少期</h3>
+          <p>幼少期の説明</p>
         </div>
         <div className="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
           <img src="img/img13.png" className="img-fluid" alt="img" />
@@ -20,8 +19,7 @@ const TimelinePage: React.FC = () => (
       <div className="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
         <div className="col-10 col-md-5 order-3 order-md-1 timeline-content">
           <h3 className=" text-light">浪人期間</h3>
-          <p>受験勉強に意味を見いだせず、ネット大喜利に没頭する。センター試験模試は2chの受験板でネタバレ情報をGETし全ての番号を暗記して8割とっているように見せかけて親への体裁を保っていた。
-        </p>
+          <p>浪人期間について</p>
         </div>
         <div className="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
           <img src="img/img13.png" className="img-fluid" alt="img" />
@@ -33,9 +31,12 @@ const TimelinePage: React.FC = () => (
       <div className="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
         <div className="col-10 col-md-5 order-3 order-md-1 timeline-content">
           <h3 className=" text-light">Timeline Heading</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, eaque amet deleniti hic quas qui cumque
-            delectus aliquid, eius quia quod, quae, aliquam aspernatur facilis. Minima quod corporis dignissimos porro.
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe,
+            eaque amet deleniti hic quas qui cumque delectus aliquid, eius quia
+            quod, quae, aliquam aspernatur facilis. Minima quod corporis
+            dignissimos porro.
+          </p>
         </div>
         <div className="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
           <img src="img/img13.png" className="img-fluid" alt="img" />
@@ -47,9 +48,12 @@ const TimelinePage: React.FC = () => (
       <div className="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
         <div className="col-10 col-md-5 order-3 order-md-1 timeline-content">
           <h3 className=" text-light">Timeline Heading</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, eaque amet deleniti hic quas qui cumque
-            delectus aliquid, eius quia quod, quae, aliquam aspernatur facilis. Minima quod corporis dignissimos porro.
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe,
+            eaque amet deleniti hic quas qui cumque delectus aliquid, eius quia
+            quod, quae, aliquam aspernatur facilis. Minima quod corporis
+            dignissimos porro.
+          </p>
         </div>
         <div className="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
           <img src="img/img13.png" className="img-fluid" alt="img" />
@@ -60,6 +64,6 @@ const TimelinePage: React.FC = () => (
       </div>
     </div>
   </div>
-)
+);
 
-export default TimelinePage
+export default TimelinePage;
