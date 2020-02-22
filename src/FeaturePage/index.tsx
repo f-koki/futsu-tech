19a03f6c89a1fb88e05dd99ba87fe6d0f25032d5
f-koki <fukushimakoki0108@gmail.com>
2020-02-22 23:32:23 +0900

    feat: remove extra part

--- src/FeaturePage/index.tsx
+++ src/FeaturePage/index.tsx
@@ -4,14 +4,10 @@ import { MDBRow, MDBCol, MDBIcon } from "mdbreact";
 const FeaturesPage: React.FC = () => {
   return (
     <section className="my-5">
-      <h2 className="h1-responsive font-weight-bold text-center my-5">
-        Why is it so great?
-        </h2>
+      <h2 className="h1-responsive font-weight-bold text-center my-5">Works</h2>
       <p className="lead grey-text w-responsive text-center mx-auto mb-5">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-        ad minim veniam.
-        </p>
+        Engineering / Design / Keyboard
+      </p>
       <MDBRow>
         <MDBCol md="4">
           <MDBRow className="mb-3">
@@ -28,7 +24,7 @@ const FeaturesPage: React.FC = () => {
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Reprehenderit maiores nam, aperiam minima assumenda deleniti
                 hic.
-                </p>
+              </p>
             </MDBCol>
           </MDBRow>
           <MDBRow className="mb-3">
@@ -41,12 +37,16 @@ const FeaturesPage: React.FC = () => {
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Reprehenderit maiores nam, aperiam minima assumenda deleniti
                 hic.
-                </p>
+              </p>
             </MDBCol>
           </MDBRow>
           <MDBRow className="mb-3">
             <MDBCol size="2">
-              <MDBIcon icon="glass-martini" size="2x" className="deep-purple-text" />
+              <MDBIcon
+                icon="glass-martini"
+                size="2x"
+                className="deep-purple-text"
+              />
             </MDBCol>
             <MDBCol size="10">
               <h5 className="font-weight-bold mb-3">Relaxing</h5>
@@ -54,55 +54,7 @@ const FeaturesPage: React.FC = () => {
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Reprehenderit maiores nam, aperiam minima assumenda deleniti
                 hic.
-                </p>
-            </MDBCol>
-          </MDBRow>
-        </MDBCol>
-        <MDBCol md="4" className="text-name">
-          <img
-            className="img-fluid"
-            src="https://mdbootstrap.com/img/Mockups/Transparent/Small/iphone-portfolio1.png"
-            alt=""
-          />
-        </MDBCol>
-        <MDBCol md="4">
-          <MDBRow className="mb-3">
-            <MDBCol size="2">
-              <MDBIcon icon="heart" size="2x" className="deep-purple-text" />
-            </MDBCol>
-            <MDBCol size="10">
-              <h5 className="font-weight-bold mb-3">Beloved</h5>
-              <p className="grey-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Reprehenderit maiores nam, aperiam minima assumenda deleniti
-                hic.
-                </p>
-            </MDBCol>
-          </MDBRow>
-          <MDBRow className="mb-3">
-            <MDBCol size="2">
-              <MDBIcon icon="bolt" size="2x" className="deep-purple-text" />
-            </MDBCol>
-            <MDBCol size="10">
-              <h5 className="font-weight-bold mb-3">Rapid</h5>
-              <p className="grey-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Reprehenderit maiores nam, aperiam minima assumenda deleniti
-                hic.
-                </p>
-            </MDBCol>
-          </MDBRow>
-          <MDBRow className="mb-3">
-            <MDBCol size="2">
-              <MDBIcon icon="magic" size="2x" className="deep-purple-text" />
-            </MDBCol>
-            <MDBCol size="10">
-              <h5 className="font-weight-bold mb-3">Magical</h5>
-              <p className="grey-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Reprehenderit maiores nam, aperiam minima assumenda deleniti
-                hic.
-                </p>
+              </p>
             </MDBCol>
           </MDBRow>
         </MDBCol>
