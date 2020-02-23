eb4a1ac6c996b3d94194f2cd2c56aeb75954bdd2
f-koki <fukushimakoki0108@gmail.com>
2020-02-23 16:17:55 +0900

    feat: arrange design and descriptions

--- src/FeaturePage/index.tsx
+++ src/FeaturePage/index.tsx
@@ -4,9 +4,11 @@ import { MDBRow, MDBCol, MDBIcon } from "mdbreact";
 const FeaturesPage: React.FC = () => {
   return (
     <section className="my-5">
-      <h2 className="h1-responsive font-weight-bold text-center my-5">Works</h2>
+      <h2 className="h1-responsive font-weight-bold text-center my-5">
+        Skills
+      </h2>
       <p className="lead grey-text w-responsive text-center mx-auto mb-5">
-        Engineering / Design / Keyboard
+        Engineering / Keyboard / Coarching
       </p>
       <MDBRow>
         <MDBCol md="4">
@@ -19,11 +21,10 @@ const FeaturesPage: React.FC = () => {
               />
             </MDBCol>
             <MDBCol size="10">
-              <h5 className="font-weight-bold mb-3">International</h5>
+              <h5 className="font-weight-bold mb-3">Engineering</h5>
               <p className="grey-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Reprehenderit maiores nam, aperiam minima assumenda deleniti
-                hic.
+                Java / JavaScript / CSS / Sass / Reactjs / Go / PHP / GAS /
+                初心者へのメンタリング
               </p>
             </MDBCol>
           </MDBRow>
@@ -32,11 +33,9 @@ const FeaturesPage: React.FC = () => {
               <MDBIcon icon="flask" size="2x" className="deep-purple-text" />
             </MDBCol>
             <MDBCol size="10">
-              <h5 className="font-weight-bold mb-3">Experimental</h5>
+              <h5 className="font-weight-bold mb-3">Keyboardist</h5>
               <p className="grey-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Reprehenderit maiores nam, aperiam minima assumenda deleniti
-                hic.
+                ライブ活動歴あり / Youtubeピアノ講座動画30万回視聴 / 作曲
               </p>
             </MDBCol>
           </MDBRow>
@@ -49,12 +48,8 @@ const FeaturesPage: React.FC = () => {
               />
             </MDBCol>
             <MDBCol size="10">
-              <h5 className="font-weight-bold mb-3">Relaxing</h5>
-              <p className="grey-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Reprehenderit maiores nam, aperiam minima assumenda deleniti
-                hic.
-              </p>
+              <h5 className="font-weight-bold mb-3">Coarching</h5>
+              <p className="grey-text">独立支援 / 転職支援</p>
             </MDBCol>
           </MDBRow>
         </MDBCol>
