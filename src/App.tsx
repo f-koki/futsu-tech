d92260858c3163cdb202839dcd5c80894fdaad86
f-koki <57610613+f-koki@users.noreply.github.com>
2020-02-25 20:53:52 +0900

    feat: hide timeline

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App: React.FC = () => (
     <Header />
     <body>
       <Logo />
-      <TimelinePage />
+      {/* <TimelinePage /> */}
       <FeaturesPage />
       <Footer />
     </body>
