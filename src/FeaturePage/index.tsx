0bd2d186ee6eb589b1218cafb4bab345f277aca7
f-koki <57610613+f-koki@users.noreply.github.com>
2020-02-25 21:04:46 +0900

    feat: change sizes

--- src/FeaturePage/index.tsx
+++ src/FeaturePage/index.tsx
@@ -5,13 +5,10 @@ const FeaturesPage: React.FC = () => {
   return (
     <section className="my-5">
       <h2 className="h1-responsive font-weight-bold text-center my-5">
-        Skills
+        Works
       </h2>
-      <p className="lead grey-text w-responsive text-center mx-auto mb-5">
-        Engineering / Keyboard / Coarching
-      </p>
       <MDBRow>
-        <MDBCol md="4">
+        <MDBCol md="10">
           <MDBRow className="mb-3">
             <MDBCol size="2">
               <MDBIcon
