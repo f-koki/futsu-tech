df812bdf1bb0ad8d71d4d21ed75cb1094db7f536
f-koki <fukushimakoki0108@gmail.com>
2020-03-01 23:39:49 +0900

    feat: add timeline

--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -4,6 +4,8 @@ import { Navbar } from "react-bootstrap";
 const Header: React.FC = () => (
   <Navbar fixed="top">
     <Navbar.Brand>FUTSU TECH</Navbar.Brand>
+    {/* TODO: SPAで切り替える */}
+    <Navbar.Text>略歴</Navbar.Text>
   </Navbar>
 );
 
